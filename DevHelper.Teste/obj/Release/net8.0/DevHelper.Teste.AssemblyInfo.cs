3c27aa3de977c15462d7251b67d408c21a40794d
Gabriel Collopy <gabrielcollopy96@gmail.com>
2024-11-02 15:31:20 -0300

    sla

--- DevHelper.Teste/obj/Release/net8.0/DevHelper.Teste.AssemblyInfo.cs
+++ DevHelper.Teste/obj/Release/net8.0/DevHelper.Teste.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevHelper.Teste")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+410b9dcbfd9b55b4d4cf3f2b3fdeafdcbeb77fd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevHelper.Teste")]
